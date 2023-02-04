4906a1cd5bbf259238600943e5784ae9f519e32a
Winnie <zxc898977@gmail.com>
2023-02-05 00:56:30 +0800

    getting started  #12
    Signed-off-by: Winnie <zxc898977@gmail.com>

--- src/mahjong/GameInitial.py
+++ src/mahjong/GameInitial.py
@@ -12,16 +12,29 @@
 # e,s,w,n,r,g,w 東南西北中(red)發(green)白(white)
 # spring,summmer,autumn,winter,plum,orchid,chrysanthemum,bamboo 春夏秋冬梅蘭菊竹
 
+# 牌池 = Array[牌]
+# class 牌
+# class 數序牌:
+#     繼承 class牌
+# class 花牌:
+#     繼承 class牌
+# class 字牌:
+#     繼承 class牌
+
+# s_w1
+# b_p
+# name rule
+
 
 # 麻將屬性
 #   每位玩家名字 [p1,p2,p3,p4]
-#   牌池剩餘張數 default 144
-#   排池剩餘順序:[w1,w2,w3,w4,w5,w6,w7,.....]
-#   東風位 p1
+#   牌池剩餘張數 default 144 (蓋著)
+#   排池剩餘順序:[w1,w2,w3,w4,w5,w6,w7,.....] #管理員144排剩下哪些排 #存str/obj
+#   東風位 p1 (w1,w2,w3,w4) ->16
 #   南風位 p2
 #   西風位 p4
 #   北風位 p3
-#   莊家 p1
+#   莊家 p1 +1張
 # 玩家屬性
 #   玩家手牌
 class Mahjong:
@@ -32,5 +45,9 @@ class Mahjong:
         self.east_position = p1
         self.south_position = p2
         self.west_position = p4
-        self.north_position = p3
+        self.north_posit
+
+    def draw_card(self):
+        self.cards_remaining -= 1
+        self.cards_remaining_order
         self.banker = p1
